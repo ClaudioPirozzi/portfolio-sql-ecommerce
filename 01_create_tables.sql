529124059688c91978eacc7dc025eacf24a9a9bf
Claudio Vinicio Pirozzi Alves <104874049+ClaudioPirozzi@users.noreply.github.com>
2026-02-04 11:22:28 -0300

    Add files via upload

---
+++ 01_create_tables.sql
@@ -0,0 +1,68 @@
+-- =========================
+-- PROJETO PORTFÓLIO: E-commerce Analytics (MySQL)
+-- =========================
+
+DROP DATABASE IF EXISTS portfolio_sql;
+CREATE DATABASE portfolio_sql;
+USE portfolio_sql;
+
+-- ---------- Customers ----------
+CREATE TABLE customers (
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  full_name   VARCHAR(120) NOT NULL,
+  email       VARCHAR(150) NOT NULL UNIQUE,
+  city        VARCHAR(80)  NOT NULL,
+  state       CHAR(2)      NOT NULL,
+  created_at  DATE         NOT NULL
+);
+
+-- ---------- Products ----------
+CREATE TABLE products (
+  product_id  INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(140) NOT NULL,
+  category     VARCHAR(60)  NOT NULL,
+  price        DECIMAL(10,2) NOT NULL,
+  active       TINYINT(1) NOT NULL DEFAULT 1,
+  created_at   DATE NOT NULL
+);
+
+-- ---------- Orders ----------
+CREATE TABLE orders (
+  order_id     INT AUTO_INCREMENT PRIMARY KEY,
+  customer_id  INT NOT NULL,
+  order_date   DATE NOT NULL,
+  status       ENUM('paid','shipped','delivered','canceled') NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- ---------- Order Items ----------
+CREATE TABLE order_items (
+  order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+  order_id      INT NOT NULL,
+  product_id    INT NOT NULL,
+  quantity      INT NOT NULL,
+  unit_price    DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+-- ---------- Payments (1 por pedido aqui) ----------
+CREATE TABLE payments (
+  payment_id   INT AUTO_INCREMENT PRIMARY KEY,
+  order_id     INT NOT NULL UNIQUE,
+  payment_method ENUM('pix','credit_card','boleto') NOT NULL,
+  paid_at      DATE NULL,
+  amount       DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
+-- ---------- Shipments (1 por pedido aqui) ----------
+CREATE TABLE shipments (
+  shipment_id  INT AUTO_INCREMENT PRIMARY KEY,
+  order_id     INT NOT NULL UNIQUE,
+  shipped_at   DATE NULL,
+  delivered_at DATE NULL,
+  carrier      VARCHAR(60) NULL,
+  freight      DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
---
+++ 01_kpis.sql
@@ -0,0 +1,39 @@
+/* =========================================================
+   PERGUNTA DE NEGÓCIO:
+   Qual foi a receita bruta mensal baseada nos itens vendidos,
+   desconsiderando pedidos cancelados?
+   ========================================================= */
+
+SELECT
+       DATE_FORMAT(o.order_date, '%Y-%m') AS revenue_month,
+       ROUND(SUM(oi.quantity * oi.unit_price), 2) AS revenue
+  FROM orders AS o
+        LEFT JOIN order_items AS oi 
+		ON oi.order_id = o.order_id
+  WHERE o.status <> 'canceled'
+  GROUP BY DATE_FORMAT(o.order_date, '%Y-%m')
+  ORDER BY revenue_month;
+  
+  
+  /* =========================================================
+   PERGUNTA DE NEGÓCIO:
+   Qual foi o ticket médio mensal?
+   ========================================================= */
+
+WITH order_totals AS (
+  SELECT
+    o.order_id,
+    o.order_date,
+    SUM(oi.quantity * oi.unit_price) AS order_total
+    FROM orders AS o
+		 JOIN order_items AS oi 
+         ON oi.order_id = o.order_id
+   WHERE o.status <> 'canceled'
+   GROUP BY o.order_id, o.order_date
+)
+SELECT
+  DATE_FORMAT(order_date, '%Y-%m') AS y_month,
+  ROUND(AVG(order_total), 2) AS avg_ticket
+FROM order_totals
+GROUP BY DATE_FORMAT(order_date, '%Y-%m')
+ORDER BY y_month;
