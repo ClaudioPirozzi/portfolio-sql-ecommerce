3cb28fbde36d3a796ba56f707f2562c1ab057c44
Claudio Vinicio Pirozzi Alves <104874049+ClaudioPirozzi@users.noreply.github.com>
2026-02-04 11:48:10 -0300

    Update 01_kpis.sql

--- queries /01_kpis.sql
+++ queries /01_kpis.sql
@@ -23,9 +23,9 @@ SELECT
 
 WITH order_totals AS (
   SELECT
-    o.order_id,
-    o.order_date,
-    SUM(oi.quantity * oi.unit_price) AS order_total
+         o.order_id,
+         o.order_date,
+         SUM(oi.quantity * oi.unit_price) AS order_total
     FROM orders AS o
 		 JOIN order_items AS oi 
          ON oi.order_id = o.order_id
@@ -33,8 +33,8 @@ WITH order_totals AS (
    GROUP BY o.order_id, o.order_date
 )
 SELECT
-  DATE_FORMAT(order_date, '%Y-%m') AS y_month,
-  ROUND(AVG(order_total), 2) AS avg_ticket
-FROM order_totals
-GROUP BY DATE_FORMAT(order_date, '%Y-%m')
-ORDER BY y_month;
+       DATE_FORMAT(order_date, '%Y-%m') AS y_month,
+       ROUND(AVG(order_total), 2) AS avg_ticket
+  FROM order_totals
+ GROUP BY DATE_FORMAT(order_date, '%Y-%m')
+ ORDER BY y_month;
