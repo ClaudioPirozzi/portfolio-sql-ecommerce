493268a1bc7d5a3812fd1d86543ab715530c2972
Claudio Vinicio Pirozzi Alves <104874049+ClaudioPirozzi@users.noreply.github.com>
2026-02-04 11:46:02 -0300

    Update 02_joins_left_join.sql

--- queries /02_joins_left_join.sql
+++ queries /02_joins_left_join.sql
@@ -1,1 +1,30 @@
 
+/* =========================================================
+   PERGUNTA DE NEGÓCIO:
+   Quais clientes nunca compraram?
+   ========================================================= */
+
+SELECT c.full_name, 
+	   c.email,
+       c.city,
+       o.order_id
+  FROM customers AS c
+       LEFT JOIN orders AS o 
+       ON c.customer_id = o.customer_id
+ WHERE o.order_id IS NULL
+ ORDER BY full_name;
+ 
+ /* =========================================================
+   PERGUNTA DE NEGÓCIO:
+   Produtos nunca vendidos?
+   ========================================================= */
+   
+   SELECT  p.product_id,
+           p.product_name,
+           p.category
+   v FROM products AS p
+	     LEFT JOIN order_items AS oi
+         ON oi.product_id = p.product_id
+	WHERE oi.order_item_id IS NULL 
+    ORDER BY p.product_name;
+    
